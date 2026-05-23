c628d38adf6cb2c09fe713fd227f8be0c2597bd8
Francesco Stablum <2955615+stablum@users.noreply.github.com>
2026-05-24 00:40:11 +0200

    0.0.5 Fix VS Code UI font settings

--- install-vscode.ps1
+++ install-vscode.ps1
@@ -1,4 +1,11 @@
 scoop install vscode
 code --install-extension openai.chatgpt
 code --install-extension subframe7536.custom-ui-style
+code --install-extension vscodevim.vim
+
+# code --install-extension 2048Labs.linesight
+$linesight_path = Join-Path $HOME '\Projects\LineSight\linesight-latest.vsix'
+code --install-extension $linesight_path
+
+code --list-extensions --show-versions
 
